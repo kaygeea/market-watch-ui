0642a6cd78db4e5b81163e4f633373e55294a068
Koyejo Adinlewa <adinlewakoyejo@yahoo.com>
2026-06-18 19:19:27 +0100

    fix: update title signal in App component to 'Market Watch'

--- projects/market-watch-frontend/src/app/app.component.ts
+++ projects/market-watch-frontend/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class App {
-  protected readonly title = signal('market-watch-frontend');
+  protected readonly title = signal('Market Watch');
 }
