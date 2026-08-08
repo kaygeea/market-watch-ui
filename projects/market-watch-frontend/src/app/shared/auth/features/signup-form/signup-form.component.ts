5794d6103b8074bce56270534f0cd8333fa8de89
Kaygeea <adinlewakoyejo@yahoo.com>
2026-08-08 03:56:25 +0100

    feat(signup): add signup form component with template and styles

---
+++ projects/market-watch-frontend/src/app/shared/auth/features/signup-form/signup-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-fe-signup-form',
+  imports: [],
+  templateUrl: './signup-form.component.html',
+  styleUrl: './signup-form.component.css',
+})
+export class SignupForm {}
